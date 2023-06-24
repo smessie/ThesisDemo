7c9c86e113bacd3111d1ff2968a5dd8eae5cd93e
smessie <smessie@smessie.com>
2023-06-24 20:00:34 +0200

    feat: Query ratings every 3 seconds

--- src/components/ThesisDemo.vue
+++ src/components/ThesisDemo.vue
@@ -1,10 +1,54 @@
 <template>
-<h1>Hello!</h1>
+  <h1>Hello!</h1>
 </template>
 
 <script>
+import {QueryEngine} from "@comunica/query-sparql";
+
 export default {
-  name: "ThesisDemo"
+  name: "ThesisDemo",
+  data() {
+    return {
+      engine: new QueryEngine(),
+      ratings : [],
+    }
+  },
+  async created() {
+    // Execute every 3 seconds
+    setInterval(async () => {
+      await this.queryResource();
+    }, 3000);
+  },
+  methods: {
+    async queryResource() {
+      const data = await fetch('https://solid.smessie.com/thesis/forms/thesis-rating.ttl', {
+        cors: "cors",
+      }).then((response) => response.text());
+
+      const bindings = await (await this.engine.queryBindings(`
+        PREFIX schema: <http://schema.org/>
+        SELECT ?value ?explanation WHERE {
+            ?s a schema:Rating ;
+                schema:ratingValue ?value ;
+                schema:ratingExplanation ?explanation .
+        }
+      `, {
+            sources: [{
+              type: 'stringSource',
+              value: data,
+              mediaType: 'text/turtle',
+            }]
+          }
+      )).toArray();
+
+      this.ratings = bindings.map((binding) => {
+        return {
+          value: binding.get('value').value,
+          explanation: binding.get('explanation').value,
+        }
+      });
+    }
+  },
 }
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    devSourcemap: true,
-  },
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    css: {
+        devSourcemap: true,
+    },
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    // https://github.com/jonathanpmartins/v-money3/issues/70#issuecomment-1284503693
+    optimizeDeps: {
+        esbuildOptions: {
+            target: ["es2020", "safari14"],
+        },
+    },
+    build: {
+        target: ["es2020", "safari14"],
+    },
+    define: {
+        // https://github.com/permafrost-dev/vue-ray/issues/3#issuecomment-1055944939
+        global: "window",
+        "process.env": {},
+    },
 })
